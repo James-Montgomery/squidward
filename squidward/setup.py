9a2998cc2315d4bc3bc7ed4a3958d2b449a1bf9e
James Montgomery <jamesmontgomery@Jamess-MacBook-Pro-2.local>
2018-12-06 15:43:56 -0500

    committing before transition to squidward

---
+++ squidward/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='squidward',
+      version='0.1',
+      description='Package for implementing Gaussian Process models',
+      url='TBD',
+      author='James Montgomery',
+      author_email='DontEmailMe@fake.com',
+      license='MIT',
+      packages=['squidward'],
+      zip_safe=False)
