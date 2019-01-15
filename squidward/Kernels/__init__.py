4114b17a22f52f326001caa1b583a1f67805fc82
James Montgomery <jamesmontgomery@Jamess-MacBook-Pro-2.local>
2019-01-15 14:21:44 -0500

    fixing capitalization errors

--- squidward/Kernels/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-This script contains code for creating kernels for
-gaussian processes modeling.
-"""
-
-import squidward.kernels.kernel_base
-import squidward.kernels.distance
--- squidward/gp_viz/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-This script contains code for creating useful visualizations for gaussian
-process modeling work. Visualization functions are grouped into regression and
-classification modules.
-"""
-
-import squidward.gp_viz.common
-import squidward.gp_viz.regression
-import squidward.gp_viz.classification
